67573cc496afc06fe7abb13d12c3a55234a451d2
adaya-elias-gracia <adaeligra@alu.edu.gva.es>
2023-02-20 22:36:32 +0100

    Documentar es de sabios, aunque leerla sea pesado

---
+++ geometria/src/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_AEG"},{"p":"<Unnamed>","l":"FiguraGeometrica_AEG"},{"p":"<Unnamed>","l":"Rectangulo_AEG"},{"p":"<Unnamed>","l":"Triangulo_AEG"}];updateSearchResults();
